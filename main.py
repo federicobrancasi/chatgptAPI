e2b5135d3b46eab68f12519549c106c06719c731
Federico Brancasi <federicobrancasi@gmail.com>
2023-03-05 03:07:05 +0100

    Working chatGPT response

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import openai
+
+# import of api key
+with open("apikey.json") as f:
+    jsonFile = json.load(f)
+    api_key = jsonFile["api_key"]
+
+# set api key
+openai.api_key = api_key
+
+
+# function to get response from gpt-3
+def get_response(messages_list: list):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages_list,
+        temperature=1.0  # from 0.0 to 2.0
+    )
+    return response
+
+
+# main
+if __name__ == "__main__":
+    messages = [
+        {"role": "system", "content": "Sei un assistente virtuale chiamata Alice e la tua lingua è l'italiano."},
+    ]
+    user_input = input("\nTu: ")
+    messages.append({"role": "user", "content": user_input})
+    new_message = get_response(messages)
+    print(new_message)
